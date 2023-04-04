2796cbf453151787323dfd27e8cfb8d52d974216
EricMungai97 <mungaikinus@gmail.com>
2023-04-03 18:57:23 -0700

    complete

--- src/components/copykitt.tsx
+++ src/components/copykitt.tsx
@@ -1,19 +1,25 @@
-import React, { useState } from "react"
+import React from "react";
 import Form from "./form";
 import Results from "./results";
-const CopyKitt: React.FC = () => {
+import Image from "next/image";
+import logo from "/public/copykittLogo.svg";
 
-  const [prompt, setPrompt] = useState("");
-  const [snippet, setSnippet] = useState("");
-  const [keywords, setKeywords] = useState([]);
-  const [hasResult, setHasResult] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+const CopyKitt: React.FC = () => {
   const CHARACTER_LIMIT: number = 32;
-  const ENDPOINT: string = "https://copykitt-three.vercel.app/generate_snippets_and_keywords?"
+  const ENDPOINT: string =
+    "https://copykitt-api.vercel.app/generate_snippets_and_keywords";
+  const [prompt, setPrompt] = React.useState("");
+  const [snippet, setSnippet] = React.useState("");
+  const [keywords, setKeywords] = React.useState([]);
+  const [hasResult, setHasResult] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
+
   const onSubmit = () => {
-    console.log("Submitting : " + prompt);
+    console.log("Submitting: " + prompt);
     setIsLoading(true);
-    fetch(`${ENDPOINT}prompt=${prompt}`).then((res) => res.json()).then(onResult);
+    fetch(`${ENDPOINT}?prompt=${prompt}`)
+      .then((res) => res.json())
+      .then(onResult);
   };
 
   const onResult = (data: any) => {
@@ -30,23 +36,48 @@ const CopyKitt: React.FC = () => {
   };
 
   let displayedElement = null;
+
   if (hasResult) {
-    displayedElement = <Results snippet={snippet} keywords={keywords} onBack={onReset} prompt={prompt} />
+    displayedElement = (
+      <Results
+        snippet={snippet}
+        keywords={keywords}
+        onBack={onReset}
+        prompt={prompt}
+      />
+    );
   } else {
-    displayedElement = <Form
-      prompt={prompt}
-      setPrompt={setPrompt}
-      onSubmit={onSubmit}
-      isLoading={isLoading}
-      characterLimit={CHARACTER_LIMIT} />;
+    displayedElement = (
+      <Form
+        prompt={prompt}
+        setPrompt={setPrompt}
+        onSubmit={onSubmit}
+        isLoading={isLoading}
+        characterLimit={CHARACTER_LIMIT}
+      />
+    );
   }
 
-  return (
-    <>
-      <h1>CopyKitt!</h1>
-      {displayedElement}
-    </>
-  )
-}
+  const gradientTextStyle =
+    "text-white text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-blue-500 font-light w-fit mx-auto";
 
-export default CopyKitt
+  return (
+    <div className="h-screen flex">
+      <div className="max-w-md m-auto p-2">
+        <div className="bg-slate-800 p-6 rounded-md text-white">
+          <div className="text-center my-6">
+            <Image src={logo} width={42} height={42} alt="" className="mx-auto" />
+            <h1 className={gradientTextStyle + " text-3xl font-light"}>
+              CopyKitt
+            </h1>
+            <div className={gradientTextStyle}>Your AI branding assistant</div>
+          </div>
+
+          {displayedElement}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CopyKitt;
