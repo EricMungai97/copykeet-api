e634ebd5d1e914a53bf6a75241861149d3977d7f
Eric Mungai Kinuthia <110691414+EricMungai97@users.noreply.github.com>
2023-12-10 13:27:09 -0800

    Update copykitt_api.py

--- app/copykitt_api.py
+++ app/copykitt_api.py
@@ -56,7 +56,7 @@ def generate_keywords(prompt: str) -> list[str]:
     enriched_prompt = f"Generate related branding keywords for {prompt}:"
     print(enriched_prompt)
 
-    response = openai.Completion.create(model="text-davinci-003", prompt=enriched_prompt, temperature=0, max_tokens=32)
+    response = openai.Completion.create(model="gpt-3.5-turbo-instruct", prompt=enriched_prompt, temperature=0, max_tokens=32)
 
     # Extract output text.
     keywords_text: str = response["choices"][0]["text"]
